2c9c1266e14b7783bb40f5795c9fc5967d42658d
shuhei <68697620+shuhei1110@users.noreply.github.com>
2024-09-07 21:56:45 +0900

    fix :art: Fix bag that can not install "PlugInstaller" at setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 
-URL = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs "$URL"
